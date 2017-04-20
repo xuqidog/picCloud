f2227ef918a0933946f1953aca2768957f3a7b06
xuqidong <xuqidong@xuqidong.local>
2017-04-20 10:47:52 +0800

    picCloud
    图云

---
+++ picCloud.py
@@ -0,0 +1,16 @@
+#encoding=utf-8
+import matplotlib.pyplot as plt 
+from pylab import *
+from wordcloud import WordCloud 
+import jieba 
+
+text_from_file_with_apath = open('/Users/xuqidong/picCloud/coco.txt').read() 
+wordlist_after_jieba = jieba.cut(text_from_file_with_apath, cut_all = True) 
+wl_space_split = " ".join(wordlist_after_jieba) 
+backgroud_Image = plt.imread('timg.jpg')
+my_wordcloud = WordCloud(font_path='/Users/xuqidong/picCloud/SimHei.ttf',background_color = 'white',mask = backgroud_Image,)
+my_wordcloud.generate(wl_space_split) 
+plt.imshow(my_wordcloud) 
+plt.axis("off") 
+plt.show()
+
